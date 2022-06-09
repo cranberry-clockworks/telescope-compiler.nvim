56c75126df26edc86c31787781050b209b9548f6
Aleksei Abakumkin <aleksei.abakumkin@abbyy.com>
2022-06-09 13:38:33 +0300

    Implement simple plugin for Telescope

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # telescope-compiler
-A plugin for telescope.nvim to select compiler
+
+A plugin for telescope.nvim to select compiler.
+
+## Installation
+
+### Packer
+
+Add following to the packer config:
+```lua
+use {
+    'cranberry-knight/telescope-compiler.nvim',
+    require = 'telescope.nvim',
+}
+```
+
+## Setup
+
+Add following lines to the telescope config:
+```lua
+require('telescope').load_extension('compiler')
+```
+
+## Use
+
+```
+:Telescope compiler
+```
---
+++ lua/telescope/_extensions/compiler.lua
@@ -0,0 +1,35 @@
+﻿local telescope = require('telescope')
+local pickers = require('telescope.pickers')
+local finders = require('telescope.finders')
+local config = require('telescope.config').values
+local actions = require('telescope.actions')
+local state = require('telescope.actions.state')
+
+local function compiler(opts)
+    local compilers = vim.fn.getcompletion("", 'compiler')
+    pickers.new(opts, {
+        prompt_title = "Compilers",
+        finder = finders.new_table({
+            results = compilers
+        }),
+        sorter = config.generic_sorter(opts),
+        attach_mappings = function(prompt_buffer)
+            actions.select_default:replace(function ()
+                local selection = state.get_selected_entry()
+                if (selection == nil) then
+                    print("[telescope] Nothing currently selected")
+                    return
+                end
+                actions.close(prompt_buffer)
+                vim.cmd(string.format('compiler %s', selection[1]))
+            end)
+            return true
+        end,
+    }):find()
+end
+
+return telescope.register_extension({
+    exports = {
+        compiler = compiler
+    }
+})
